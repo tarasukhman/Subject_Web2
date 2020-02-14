5563fbdb9468f301f0fafe2dbe41716fa6ee6f08
tarasukhman <ukhmantaras.18.2000@gmail.com>
2020-02-15 01:27:54 +0200

    Task1 was created

---
+++ task1.js
@@ -0,0 +1,8 @@
+var string_separate = function(str) {
+    var array = [];
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] != ' ') {
+            array.push(str[i]);
+        }
+    }
+}
