a02593e67afe8fbd751539714bf21da89ec5eeaf
tarasukhman <ukhmantaras.18.2000@gmail.com>
2020-02-15 01:34:59 +0200

    Almost finished func

--- task1.js
+++ task1.js
@@ -1,8 +1,16 @@
 var string_separate = function(str) {
     var array = [];
+    var word = "";
     for (var i = 0; i < str.length; i++) {
         if (str[i] != ' ') {
-            array.push(str[i]);
+            word += str[i];
+        }
+        if (str[i] == ' ') {
+            array.push(word);
+            word = '';
         }
     }
+    return array;
 }
+
+console.log(string_separate("hello i am taras"));
