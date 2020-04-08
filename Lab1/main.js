96878b7513d97bf632b4a733cac054237615ce71
tarasukhman <ukhmantaras.18.2000@gmail.com>
2020-04-08 15:56:43 +0300

    Done

--- Lab1/main.js
+++ Lab1/main.js
@@ -9,12 +9,14 @@ var task5 = require('./task5.js')
 var task6 = require('./task6.js')
 
 
-console.log('Task1:\n'+task1.string_separate("This is some text")+'\n');
-console.log('Task2:\n'+task2.StringSort("programmig")+'\n');
-console.log('Task3:\n'+task3.MaxDivisor(28, 28)+'\n');
-console.log('Task4:\n'+task4.RandomArrayShuffle([1, 5, 3, 4, 8, 7])+'\n')
-console.log('Task5:\n'+task5.GetDaysInMonth(2, 2020)+'\n');
-console.log('Task6:\n'+task6.User.sort(task6.compare)+'\n');
+console.log('Task1:\n' + task1.string_separate("This is some text") + '\n');
+console.log('Task2:\n' + task2.StringSort("programmig") + '\n');
+console.log('Task3:\n' + task3.MaxDivisor(28, 28) + '\n');
+console.log('Task4:\n' + task4.RandomArrayShuffle([1, 5, 3, 4, 8, 7]) + '\n')
+console.log('Task5:\n' + task5.GetDaysInMonth(2, 2020) + '\n');
+console.log('Task6:\n')
+console.log(task6.User.sort(task6.compare))
+console.log('\n');
 /*sortedUsers = task6.User.sort(task6.compare);
 sortedUsers.forEach(e => {
     console.log(e.name);
